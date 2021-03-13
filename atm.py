33515065657425511113cfa776d4bef2751b6226
Darko Anastasov <anastasovdarko95@gmail.com>
2021-03-13 11:51:16 +0100

    Initial commit

---
+++ atm.py
@@ -0,0 +1,14 @@
+import time
+
+print('Please insert your CARD')
+
+time.sleep(5)
+
+password = 1234
+
+pin = int(input('Enter your pin'))
+
+if pin == password:
+    
+else:
+    print('Wrong pin. Please try again')
